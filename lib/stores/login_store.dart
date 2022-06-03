5df0beeee674fc343767a2e3f69010fe531ab3f1
Art0ur <Arrthurus@gmail.com>
2022-06-03 01:48:35 -0300

    Validacao login

--- lib/stores/login_store.dart
+++ lib/stores/login_store.dart
@@ -26,7 +26,7 @@ abstract class _LoginStore with Store {
 
   @computed
   bool get passwordValid => password != null && password.length >= 4;
-  String? get passwordError => passwordValid ? null : 'Senhaa invalida';
+  String? get passwordError => passwordValid ? null : 'Senha invalida';
 
   //Function() get signUpPressed => (isFormValid && !loading) ? _signUp : _signUp;
   @computed
